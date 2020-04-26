fd59682695ab593fe26e8eb74b6c7c71c9acbaaf
Erik Steggall <erik.stegall@renovo.auto>
2020-04-25 22:19:39 -0700

    Fixed read_file, added errorhandler

--- read_file.rs
+++ read_file.rs
@@ -3,7 +3,15 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn read_file_lines(filename: &Path) -> io::Result<io::Lines<io::BufReader<File>>> {
+fn print_lines(filelines: io::Lines<io::BufReader<File>>) {
+    for line in filelines {
+        if let Ok(ip) = line {
+              println!("{}", ip);
+         }
+    } 
+}
+
+fn open_file(filename: &Path) -> io::Result<io::Lines<io::BufReader<File>>> {
     let _filehandle = File::open(filename)?;
     Ok(io::BufReader::new(_filehandle).lines())
 
@@ -11,19 +19,16 @@ fn read_file_lines(filename: &Path) -> io::Result<io::Lines<io::BufReader<File>>
 
 fn read_file(filename: &Path) {
     println!("File name is {}", filename.display());
-    if let Ok(_lines) = read_file_lines(filename){
-        for line in _lines {
-            if let Ok(ln) = line {
-                println!("{}", ln);
-            }
-        }
-        println!("Opened file");
-    } else {
-        eprintln!("Error, failed to open file {}", std::io::Result());
-    }  
+    let _lines = match open_file(filename){
+        Ok (_lines) => 
+             print_lines(_lines),
+        Err(_e) =>
+            eprintln!("Error, failed to open file {}", _e),
+    };
 }
+
 fn main() {
-    let test_filename_string = String::from("~/scratch/grateful_dead/blah5.config");
+    let test_filename_string = String::from("/home/egall/scratch/greatful_dead/blah5.config");
     let test_filename = Path::new(&test_filename_string);
     println!("Testing read_file()");
     read_file(&test_filename);
